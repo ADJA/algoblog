23e876905a99e20a08285fbab56a1f3324b7903e
Adilet <adilet@adilet.org>
2019-11-19 23:58:38 +0600

    Modified batch scriipt

--- Misc/StressTesting/stress.bat
+++ Misc/StressTesting/stress.bat
@@ -1,19 +1,22 @@
 @echo off
 
-:loop
-  g++ solution.cpp -o solution.exe
-  g++ stupid.cpp -o stupid.exe
-  g++ gen.cpp -o gen.exe
+g++ solution.cpp -o solution.exe
+g++ stupid.cpp -o stupid.exe
+g++ gen.cpp -o gen.exe
 
-  gen.exe > in
+set i=0
+
+:loop
+  gen.exe %i% > in
   solution.exe < in > out
   stupid.exe < in > out-stupid
-  
+
   fc out out-stupid
   if errorlevel 1 
     goto fail
-  
+
   echo OK
+  set /a i=%i%+1
   goto loop
 
 :fail
